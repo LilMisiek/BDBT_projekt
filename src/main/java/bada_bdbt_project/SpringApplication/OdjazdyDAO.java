ac24d06b4e32cef1df6fe86639ef84b598e9235d
lil_misiek <123581072+LilMisiek@users.noreply.github.com>
2025-03-28 17:14:55 +0100

    poprawka

--- src/main/java/bada_bdbt_project/SpringApplication/OdjazdyDAO.java
+++ src/main/java/bada_bdbt_project/SpringApplication/OdjazdyDAO.java
@@ -23,7 +23,7 @@ public class OdjazdyDAO {
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
 
-    // Pobieranie wszystkich odjazdów
+
     public List<Odjazdy> findAll() {
         String sql = "SELECT odjazdy.*, przystanki.nazwa_przystanku " +
                 "FROM odjazdy " +
@@ -33,7 +33,7 @@ public class OdjazdyDAO {
         return namedParameterJdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Odjazdy.class));
     }
 
-    // Metoda do filtrowania odjazdów na podstawie linii i przystanków
+
     public List<Odjazdy> findRozklad(List<String> selectedLinie, List<Integer> selectedPrzystanki) {
         StringBuilder sql = new StringBuilder(
                 "SELECT o.nr_odjazdu, o.nr_linii, p.nazwa_przystanku, " +
@@ -47,22 +47,22 @@ public class OdjazdyDAO {
 
         MapSqlParameterSource params = new MapSqlParameterSource();
 
-        // Dodanie warunku dla wybranych linii, jeśli istnieją
+   
         if (selectedLinie != null && !selectedLinie.isEmpty()) {
             sql.append(" AND l.nr_linii IN (:linie)");
             params.addValue("linie", selectedLinie);
         }
 
-        // Dodanie warunku dla wybranych przystanków, jeśli istnieją
+
         if (selectedPrzystanki != null && !selectedPrzystanki.isEmpty()) {
             sql.append(" AND p.nr_przystanku IN (:przystanki)");
             params.addValue("przystanki", selectedPrzystanki);
         }
 
-        // Opcjonalnie: Sortowanie wyników, np. według godziny odjazdu
+
         sql.append(" ORDER BY o.godzina ASC");
 
-        // Wykonanie zapytania i mapowanie wyników
+
         return namedParameterJdbcTemplate.query(sql.toString(), params, BeanPropertyRowMapper.newInstance(Odjazdy.class));
     }
 
@@ -80,7 +80,7 @@ public class OdjazdyDAO {
         MapSqlParameterSource params = new MapSqlParameterSource()
                 .addValue("nrTramwaju", odjazd.getNrTramwaju())
                 .addValue("nrPrzystanku", odjazd.getNrPrzystanku())
-                .addValue("godzina", Time.valueOf(odjazd.getGodzina())) // Convert LocalTime to Time
+                .addValue("godzina", Time.valueOf(odjazd.getGodzina()))
                 .addValue("czyNaZadanie", odjazd.getCzyNaZadanie())
                 .addValue("nrOdjazdu", odjazd.getNrOdjazdu());
 
@@ -104,7 +104,7 @@ public class OdjazdyDAO {
             odjazd.setNrPrzystanku(rs.getInt("nr_przystanku"));
             Time time = rs.getTime("godzina");
             if (time != null) {
-                odjazd.setGodzina(time.toLocalTime()); // Convert Time to LocalTime
+                odjazd.setGodzina(time.toLocalTime());
             }
             odjazd.setCzyNaZadanie(rs.getString("czy_na_zadanie"));
             odjazd.setNrLinii(rs.getString("nr_linii"));
@@ -114,10 +114,10 @@ public class OdjazdyDAO {
         });
     }
 
-    // Implementacja innych metod CRUD (save, get, update, delete)
+
 
     public void save(Odjazdy odjazd) {
-        // 1. Wstawienie nowego odjazdu
+
         String insertSql = "INSERT INTO odjazdy (nr_tramwaju, nr_przystanku, godzina, czy_na_zadanie, nr_linii) " +
                 "VALUES (:nrTramwaju, :nrPrzystanku, :godzina, :czyNaZadanie, :nrLinii)";
         MapSqlParameterSource insertParams = new MapSqlParameterSource();
@@ -130,7 +130,7 @@ public class OdjazdyDAO {
 
         namedParameterJdbcTemplate.update(insertSql, insertParams);
 
-        // 2. Aktualizacja nr_linii w tabeli tramwaje
+
         String updateSql = "UPDATE tramwaje SET nr_linii = :nrLinii WHERE nr_tramwaju = :nrTramwaju";
         MapSqlParameterSource updateParams = new MapSqlParameterSource();
         updateParams.addValue("nrLinii", odjazd.getNrLinii());
@@ -139,12 +139,12 @@ public class OdjazdyDAO {
         namedParameterJdbcTemplate.update(updateSql, updateParams);
     }
 
-    /* Read – odczytywanie danych z bazy */
+  
     public Odjazdy get(int id) {
-        return getById(id); // Implemented using getById
+        return getById(id); 
     }
 
-    /* Delete – wybrany rekord z danym id */
+
     public void delete(int nrOdjazdu) {
         String sql = "DELETE FROM odjazdy WHERE nr_odjazdu = :nrOdjazdu";
         MapSqlParameterSource params = new MapSqlParameterSource("nrOdjazdu", nrOdjazdu);
